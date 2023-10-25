cf6d0684c93abe36af42f44de4459739181e233c
Abdul Jalil Tamjid <114805829+JourneyWithgithub@users.noreply.github.com>
2023-10-25 13:03:24 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let menu =document.querySelector('#menu-btn');
+let navbar=document.querySelector('.header .navbar');
+
+menu.onclick= ()=>
+{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+    
+}
+
 window.onscroll=()=>
 {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+
 
 var swiper= new Swiper(".home-slider",{
     loop: true,
@@ -11,3 +23,45 @@ var swiper= new Swiper(".home-slider",{
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoHeight: true,
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+
+let CurrentItem = 0;
+
+loadMoreBtn.onclick = () => {
+    let boxes = document.querySelectorAll('.packages .box-container .box');
+
+    for (let i = CurrentItem; i < CurrentItem + 3; i++) {
+        if (boxes[i]) {
+            // Perform actions on boxes[i] here, e.g., show them
+            boxes[i].style.display = 'inline-block';
+        }
+    }
+
+    // Update the current item count for the next load
+    CurrentItem += 3;
+
+    if (CurrentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+};
+
